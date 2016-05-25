4f3fb70196fd3ab6a65eeb70cc32b53648632452
tomassdepakin <tomassdepakin@gmail.com>
2016-05-25 21:39:52 +0300

    Initial commit

---
+++ src/main/java/com/sameperson/texasholdem/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.sameperson.texasholdem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String home() {
+        System.out.println("hi from controller");
+        return "home";
+    }
+
+}
