e8590de90c47c253090f48e9955e2c817889d9b7
Saem <ottosigma@gmail.com>
2016-05-30 17:32:04 +0300

    updated page and gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require("gulp"),
+    util = require("gulp-util")
+    rename = require('gulp-rename'),
+    log = util.log;
+
+gulp.task("default", ["angular-handling"]);
+
+gulp.task("angular-handling", function () {
+    log("Getting angular " + (new Date()).toString());
+    gulp.src("node_modules/angular/*")
+        .pipe(gulp.dest("src/main/resources/static/vendor/angular"));
+});
