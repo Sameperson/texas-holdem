e39d1953c7ad9fbd721c386f0975332c6230cb65
Saem <ottosigma@gmail.com>
2016-06-08 17:33:37 +0300

    updated js

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 $(function () {
     $(".button-collapse").sideNav({edge: 'right'});
 });
@@ -5,34 +7,25 @@ $(function () {
 var mainModule = angular.module('main', [])
 
     .controller('MainController', function MainController($scope, dataService) {
-        var me = this;
+        var app = this;
         this.hello = "Hello!";
 
         this.helloConsole = function () {
             console.log("Test");
         }
 
-        dataService.getLeaderboard(function(response) {
-            console.log("Getting data...");
-            me.leaderboard = response.data;
-        });
+        this.getLeaderboard = function() {
+            dataService.getLeaderboard(function(response) {
+                console.log("Getting data...");
+                app.leaderboard = response.data;
+            });
+        }
     })
 
     .component('loginForm', {
         templateUrl: 'components/login.html',
         controller: function () {
             this.login = "User1";
-        }
-    })
-
-    .service('dataService', function ($http) {
-        this.helloConsole = function () {
-            console.log('This is the hello console service');
-        };
-
-        this.getLeaderboard = function (callback) {
-            console.log("Getting leaderboard...")
-            $http.get('/leaderboardJSON').then(callback);
         }
     });
 
